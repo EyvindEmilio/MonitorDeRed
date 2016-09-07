cf1b1a0d644841e8cfc4ecbcc2ef02e3655f7dfb
Judyss <ju.adrianis11@gmail.com>
2016-09-07 17:56:19 -0400

    Iftop regex reconfig

--- NodeServer/iftop.js
+++ NodeServer/iftop.js
@@ -13,7 +13,7 @@ var iftop = function (onData, settings) {
     cmd = spawn('iftop', args);
     cmd.stdout.on('data', function (data) {
         data = data.toString('utf8');
-        var matches = data.match(/([\d]+ \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}[ \t]+[<>=]{1,2}[ \t]+([0-9.a-zA-Z]+[ \t]+){3}[0-9.a-zA-Z]+)\n?([\t ]+ \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}[ \t]+[<>=]{1,2}[ \t]+([0-9.a-zA-Z]+[ \t]+){3}[0-9.a-zA-Z]+)/g);
+        var matches = data.match(/([\d]+ \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}[ \t]+[<>=]{1,2}[ \t]+([0-9.,a-zA-Z]+[ \t]+){3}[0-9.,a-zA-Z]+)\n?([\t ]+ \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}[ \t]+[<>=]{1,2}[ \t]+([0-9.,a-zA-Z]+[ \t]+){3}[0-9.a-zA-Z]+)/g);
         if (matches == null)
             return;
         var list_objects = [], object;
@@ -26,6 +26,7 @@ var iftop = function (onData, settings) {
             var size_len = lines[1].match(/[0-9.]+[a-zA-Z]+/)[0];
             var len = size_len.match(/[a-zA-Z]+/)[0];
             var size = parseFloat(size_len.match(/[0-9.]+/)[0]);
+            console.log("st->" + size);
             if (len === 'b') {
                 size = size / 1024;
             } else if (len === 'Mb') {
